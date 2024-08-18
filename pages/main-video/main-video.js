a557aea90baee1b7360b976328d0ec3c84c43801
abBug <13782434+abbug@user.noreply.gitee.com>
2024-08-18 22:29:41 +0800

    下拉获取更多数据

--- pages/main-video/main-video.js
+++ pages/main-video/main-video.js
@@ -2,15 +2,28 @@
 import { getTopMV } from "../../services/video"
 Page({
   data: {
-    videoList: []
+    videoList: [],
+    offset: 0,
+    hasMore: true
   },
   onLoad() {
     this.fetchTopMV()
   },
   async fetchTopMV() {
-    const res = await getTopMV()
+    const res = await getTopMV(this.data.offset)
+    const newVideoList = [...this.data.videoList, ...res.data.data]
     this.setData({
-      videoList: res.data.data
+      videoList: newVideoList
     })
+    this.data.offset = this.data.videoList.length
+    this.data.hasMore = res.data.hasMore
+  },
+
+  onReachBottom() {
+    // 1.判断是否有更多的数据
+    if (!this.data.hasMore) return
+
+    // 2.如果有更多的数据, 再请求新的数据
+    this.fetchTopMV()
   }
 })
